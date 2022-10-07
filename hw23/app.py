e665cae274290770d856824d722c29fe5085c2c8
Alexman507 <105458434+Alexman507@users.noreply.github.com>
2022-10-07 03:42:37 +0300

    Delete hw23 directory

--- hw23/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from views import main_bp
-
-
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(main_bp)
-    return app
--- hw23/run.py
+++
@@ -1,5 +0,0 @@
-from app import create_app
-
-if name == '__main__':
-    app = create_app()
-    app.run(port=25000)
